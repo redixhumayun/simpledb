27f0b8362386506cbe9c19db3072a2c8aea274e8
Zaid Humayun <redixhumayun@gmail.com>
2025-11-27 12:06:02 +0530

    Started Page Migration
    This commit starts the migration to using the new page format. FileManager/MockFileManager internally use the new Page layout. There is a backward compatibility API provided on the new Page API to make migration easier. Blocksize is removed as parameter from file manager constructor and the compile time constant is used.

--- src/bin/simpledb-cli.rs
+++ src/bin/simpledb-cli.rs
@@ -11,7 +11,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     println!();
 
     // Initialize database
-    let db = SimpleDB::new("./simpledb-data", 1024, 8, false, 100);
+    let db = SimpleDB::new("./simpledb-data", 8, false, 100);
 
     // Main REPL loop
     loop {
